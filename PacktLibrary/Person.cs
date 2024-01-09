4f7e58e95e4f59e11ec7cacac52f77dd369f7a12
DarkProrok <Seroshtanmakcs@gmail.com>
2024-01-09 13:56:11 +0300

    Add project files.

---
+++ PacktLibrary/Person.cs
@@ -0,0 +1,72 @@
+﻿using static System.Console;
+
+namespace Packt.Shared;
+
+public class Person : object
+{
+    //поля
+    public string? Name;
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();
+
+    //методы
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born a {DateOfBirth:dddd}");
+    }
+    //создание статичного метода
+    public static Person Procreate(Person p1, Person p2)
+    {
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name}"
+    };
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+        return baby;
+    }
+    //создание метода экземпляра
+    public Person ProcreateWith(Person parthner)
+    {
+        return Procreate(this, parthner);
+    }
+
+    //реализация функционала при помощи операций
+    public static Person operator *(Person p1, Person p2)
+    {
+        return Person.Procreate(p1, p2);
+    }
+
+    public static int Factorial(int number)
+    {
+        if (number <0)
+        {
+            throw new ArgumentException($"{nameof(number)} cannot be less than zero.");
+        }
+        return localFactorial(number);
+
+        int localFactorial(int localNumber)
+        {
+            if (localNumber < 1) return 1;
+            return localNumber * localFactorial(localNumber - 1);
+        }
+    }
+    //поле делегата
+    public EventHandler? Shout;
+
+    // поле данных
+    public int AngerLevel;
+
+    //метод
+
+    public void Poke()
+    {
+        AngerLevel++;
+
+        if (AngerLevel >= 3)
+        {
+            //если что-то прослушивается...
+            Shout?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ PeopleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Packt.Shared;
+using static System.Console;
+
+Person harry = new() { Name = "Harry" };
+Person mary = new() { Name = "Mary" };
+Person jill = new() { Name = "Jill" };
+
+//использование метода экземпляра
+Person baby1 = mary.ProcreateWith(harry);
+WriteLine(baby1.Name);
+baby1.Name = "Gary";
+WriteLine(baby1.Name);
+//использование статического метода
+Person baby2 = Person.Procreate(harry, jill);
+
+//использование операции
+Person baby = harry * mary;
+WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+
+WriteLine($"{harry.Name}'s first child is named \"{harry.Children[0].Name}\"");
+
+harry.Shout = Harry_Shout;
+harry.Poke();
+harry.Poke();
+harry.Poke();
+harry.Poke();
+static void Harry_Shout(object? sender, EventArgs e)
+{
+    if (sender is null) return;
+    Person p = (Person)sender;
+    WriteLine($"{p.Name} is this angry: {p.AngerLevel}");
+}
